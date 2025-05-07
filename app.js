1efdec8ce3892922dfee4662950eb5f1aa1674bb
Amarasena <amarasenaisuru@gmail.com>
2025-05-08 02:28:33 +0530

    small change - 2

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '.next', 'server', 'pages', 'index.html'));
 });
 
-app.listen(5000, () => {
+app.listen(3000, () => {
     console.log("Server is running on port 5000");
 });
